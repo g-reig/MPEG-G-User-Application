d6436d959ff6a780ca87154e9d17967b91d6445e
Barvent <67266826+Barvent@users.noreply.github.com>
2021-04-20 18:24:00 +0200

    Added POST request to Genomic Content Service

--- user-app/src/main/java/mpegg/userapp/userapp/Controllers/FileController.java
+++ user-app/src/main/java/mpegg/userapp/userapp/Controllers/FileController.java
@@ -1,14 +1,29 @@
 package mpegg.userapp.userapp.Controllers;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class FileController {
     @PostMapping("/api/create")
-    public String uploadFiles(@RequestParam("dg_md") MultipartFile dg_md, @RequestParam("dt_md") MultipartFile[] dt_md) {
+    public String create(@RequestParam("dg_md") MultipartFile dg_md, @RequestParam("dt_md") MultipartFile[] dt_md) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("dg_md", dg_md.getResource());
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        String serverUrl = "http://localhost:8082/api/create";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8082/api/create", HttpMethod.POST, requestEntity, String.class);
         return "ok";
     }
 }
